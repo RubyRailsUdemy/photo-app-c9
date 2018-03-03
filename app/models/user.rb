0a6f2cbc498911df80c3939b84dec2b11729d7ee
Davidalen Fountain <dfountain@sacent.com>
2018-03-03 06:52:34 +0000

    Added/Tested feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  has_one :payment
+  accepts_nested_attributes_for :payment
+         
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"
